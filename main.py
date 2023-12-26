28ec2220cd94722bcd756af8226feffd6d0b4a27
MylordYimeng <2728339637@qq.com>
2023-12-26 16:32:50 +0800

    调用msoffice的api

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-import tkinter as tk
+import win32com.client as w32c
+
+
+def ppt2pdf_api(ppt_path, pdf_path):
+    powerpoint = w32c.Dispatch('PowerPoint.Application')
+    ppt = powerpoint.Presentations.Open(ppt_path,1,0,0) # ReadOnly, titled, WithoutWindow
+    ppt.SaveAs(pdf_path, 32)
+    ppt.Close()
+    powerpoint.Quit()
 
